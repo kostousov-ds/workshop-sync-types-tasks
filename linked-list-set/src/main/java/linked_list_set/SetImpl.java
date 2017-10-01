025812175121592d9b47e860e39cc4a053819ece
Denis Kostousov <kostousov-ds@bisys.ru>
2017-10-01 13:42:42 +0500

    fine-grained locking

--- linked-list-set/src/main/java/linked_list_set/SetImpl.java
+++ linked-list-set/src/main/java/linked_list_set/SetImpl.java
@@ -1,7 +1,11 @@
 package linked_list_set;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class SetImpl implements Set {
     private class Node {
+        Lock lock=new ReentrantLock();
 	Node next;
 	int x;
 
@@ -23,10 +27,15 @@ public class SetImpl implements Set {
     private Window findWindow(int x) {
 	Window w = new Window();
 	w.cur = head;
+	w.cur.lock.lock();
 	w.next = w.cur.next;
+	w.next.lock.lock();
+
 	while (w.next.x < x) {
+	    w.cur.lock.unlock();
 	    w.cur = w.next;
 	    w.next = w.cur.next;
+	    w.next.lock.lock();
 	}
 	return w;
     }
@@ -34,25 +43,35 @@ public class SetImpl implements Set {
     @Override
     public boolean add(int x) {
 	Window w = findWindow(x);
-	boolean res;
-	if (w.next.x == x) {
-	    res = false;
-	} else {
-	    w.cur.next = new Node(x, w.next);
-	    res = true;
+	try {
+	    boolean res;
+	    if (w.next.x == x) {
+		res = false;
+	    } else {
+		w.cur.next = new Node(x, w.next);
+		res = true;
+	    }
+	    return res;
+	}finally {
+	    w.cur.lock.unlock();
+	    w.next.lock.unlock();
 	}
-	return res;
     }
 
     @Override
     public boolean remove(int x) {
 	Window w = findWindow(x);
 	boolean res;
-	if (w.next.x != x) {
-	    res = false;
-	} else {
-	    w.cur.next = w.next.next;
-	    res = true;
+	try {
+	    if (w.next.x != x) {
+		res = false;
+	    } else {
+		w.cur.next = w.next.next;
+		res = true;
+	    }
+	}finally {
+	    w.cur.lock.unlock();
+	    w.next.lock.unlock();
 	}
 	return res;
     }
@@ -60,7 +79,12 @@ public class SetImpl implements Set {
     @Override
     public boolean contains(int x) {
 	Window w = findWindow(x);
-	boolean res = w.next.x == x;
-	return res;
+	try {
+	    boolean res = w.next.x == x;
+	    return res;
+	}finally {
+	    w.cur.lock.unlock();
+	    w.next.lock.unlock();
+	}
     }
 }
