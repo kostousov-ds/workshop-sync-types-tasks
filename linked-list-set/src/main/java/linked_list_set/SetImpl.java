9ee3c3858d7ba7df54be29c67d22bdb98eb06b4c
Denis Kostousov <kostousov-ds@bisys.ru>
2017-10-01 13:32:28 +0500

    coarse-grained locking

--- linked-list-set/src/main/java/linked_list_set/SetImpl.java
+++ linked-list-set/src/main/java/linked_list_set/SetImpl.java
@@ -15,53 +15,60 @@ public class SetImpl implements Set {
         Node cur, next;
     }
 
-    private final Node head = new Node(Integer.MIN_VALUE,
-        new Node(Integer.MAX_VALUE, null));
+    private final Node head = new Node(Integer.MIN_VALUE, new Node(Integer.MAX_VALUE, null));
 
     /**
      * Returns the {@link Window}, where cur.x < x <= next.x
      */
     private Window findWindow(int x) {
-        Window w = new Window();
-        w.cur = head;
-        w.next = w.cur.next;
-        while (w.next.x < x) {
-            w.cur = w.next;
-            w.next = w.cur.next;
-        }
-        return w;
+	synchronized (this) {
+	    Window w = new Window();
+	    w.cur = head;
+	    w.next = w.cur.next;
+	    while (w.next.x < x) {
+		w.cur = w.next;
+		w.next = w.cur.next;
+	    }
+	    return w;
+	}
     }
 
     @Override
     public boolean add(int x) {
-        Window w = findWindow(x);
-        boolean res;
-        if (w.next.x == x) {
-            res = false;
-        } else {
-            w.cur.next = new Node(x, w.next);
-            res = true;
-        }
-        return res;
+	synchronized (this) {
+	    Window w = findWindow(x);
+	    boolean res;
+	    if (w.next.x == x) {
+		res = false;
+	    } else {
+		w.cur.next = new Node(x, w.next);
+		res = true;
+	    }
+	    return res;
+	}
     }
 
     @Override
     public boolean remove(int x) {
-        Window w = findWindow(x);
-        boolean res;
-        if (w.next.x != x) {
-            res = false;
-        } else {
-            w.cur.next = w.next.next;
-            res = true;
-        }
-        return res;
+	synchronized (this) {
+	    Window w = findWindow(x);
+	    boolean res;
+	    if (w.next.x != x) {
+		res = false;
+	    } else {
+		w.cur.next = w.next.next;
+		res = true;
+	    }
+	    return res;
+	}
     }
 
     @Override
     public boolean contains(int x) {
-        Window w = findWindow(x);
-        boolean res = w.next.x == x;
-        return res;
+        synchronized (this) {
+	    Window w = findWindow(x);
+	    boolean res = w.next.x == x;
+	    return res;
+	}
     }
 }
