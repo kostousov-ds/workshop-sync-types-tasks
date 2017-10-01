9c89a634ccd1b0a78c62a992ea4b80f18d36b724
Denis Kostousov <kostousov-ds@bisys.ru>
2017-10-01 14:27:28 +0500

    optimistic locking

--- linked-list-set/src/main/java/linked_list_set/SetImpl.java
+++ linked-list-set/src/main/java/linked_list_set/SetImpl.java
@@ -5,8 +5,8 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class SetImpl implements Set {
     private class Node {
-        Lock lock=new ReentrantLock();
-	Node next;
+	Lock lock = new ReentrantLock();
+	volatile Node next;
 	int x;
 
 	Node(int x, Node next) {
@@ -17,6 +17,16 @@ public class SetImpl implements Set {
 
     private class Window {
 	Node cur, next;
+
+	void lock() {
+	    cur.lock.lock();
+	    next.lock.lock();
+	}
+
+	void unlock() {
+	    cur.lock.unlock();
+	    next.lock.unlock();
+	}
     }
 
     private final Node head = new Node(Integer.MIN_VALUE, new Node(Integer.MAX_VALUE, null));
@@ -27,64 +37,103 @@ public class SetImpl implements Set {
     private Window findWindow(int x) {
 	Window w = new Window();
 	w.cur = head;
-	w.cur.lock.lock();
 	w.next = w.cur.next;
-	w.next.lock.lock();
 
 	while (w.next.x < x) {
-	    w.cur.lock.unlock();
 	    w.cur = w.next;
 	    w.next = w.cur.next;
-	    w.next.lock.lock();
 	}
 	return w;
     }
 
     @Override
     public boolean add(int x) {
-	Window w = findWindow(x);
-	try {
-	    boolean res;
-	    if (w.next.x == x) {
-		res = false;
-	    } else {
-		w.cur.next = new Node(x, w.next);
-		res = true;
+	while (true) {
+	    boolean res = false;
+	    try {
+		return addImpl(x);
+	    } catch (Exception ignored) {
 	    }
-	    return res;
-	}finally {
-	    w.cur.lock.unlock();
-	    w.next.lock.unlock();
 	}
+    }
+
+    private boolean addImpl(int x) throws Exception {
+	Window w = findWindow(x);
+	w.lock();
+	try {
+	    if (validate(w, x)) {
+		boolean res;
+		if (w.next.x == x) {
+		    res = false;
+		} else {
+		    w.cur.next = new Node(x, w.next);
+		    res = true;
+		}
+		return res;
+	    } else {
+		throw new Exception("next try");
+	    }
+	} finally {
+	    w.unlock();
+	}
+    }
+
+    private boolean validate(Window w, int x) {
+	Window ww = findWindow(x);
+	return ww.cur == w.cur && ww.next == w.next && ww.cur.next == ww.next;
     }
 
     @Override
     public boolean remove(int x) {
+	while (true) {
+	    try {
+		return removeImpl(x);
+	    } catch (Exception ignored) {
+	    }
+	}
+    }
+
+    private boolean removeImpl(int x) throws Exception {
 	Window w = findWindow(x);
 	boolean res;
 	try {
+	    w.lock();
+	    if (!validate(w, x)) {
+		throw new Exception("retry");
+	    }
 	    if (w.next.x != x) {
 		res = false;
 	    } else {
 		w.cur.next = w.next.next;
 		res = true;
 	    }
-	}finally {
-	    w.cur.lock.unlock();
-	    w.next.lock.unlock();
+	} finally {
+	    w.unlock();
 	}
 	return res;
     }
 
     @Override
     public boolean contains(int x) {
+	while (true) {
+	    try {
+		return containsImpl(x);
+	    } catch (Exception ignored) {
+	    }
+	}
+    }
+
+    private boolean containsImpl(int x) throws Exception {
 	Window w = findWindow(x);
 	try {
+	    w.lock();
+	    if (!validate(w, x)) {
+		throw new Exception("retry");
+	    }
 	    boolean res = w.next.x == x;
 	    return res;
-	}finally {
-	    w.cur.lock.unlock();
-	    w.next.lock.unlock();
+	} finally {
+	    w.unlock();
 	}
     }
 }
